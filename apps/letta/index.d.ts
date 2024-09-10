6d3848eab7a01b3f90efb0b18948b4004d0dbcb5
Shubham Naik <shub@memgpt.ai>
2024-09-10 15:40:42 -0700

    chore: next

--- apps/letta/index.d.ts
+++ apps/letta/index.d.ts
@@ -10,6 +10,7 @@ declare module '*.svg' {
 }
 
 declare module '@tanstack/react-table' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     style: {
       columnAlign: 'center' | 'left' | 'right';
