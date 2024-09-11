24c0141adceff8b3273fb4582451b29543ff35d3
Shubham Naik <shub@memgpt.ai>
2024-09-11 11:11:59 -0700

    chore: fix setup script

--- scripts/configure/src/setup/generator.ts
+++ scripts/configure/src/setup/generator.ts
@@ -8,8 +8,8 @@ import type { SetupGeneratorSchema } from './schema';
 
 const LOCAL_DEFAULT_OPTIONS: SetupGeneratorSchema = {
   DATABASE_URL: 'postgresql://postgres:postgres@localhost:5432/letta',
-  GOOGLE_CLIENT_ID: 'google-client-id',
-  GOOGLE_CLIENT_SECRET: 'google-client-secret',
+  GOOGLE_CLIENT_ID: '',
+  GOOGLE_CLIENT_SECRET: '',
   GOOGLE_REDIRECT_URI: 'http://localhost:3000/api/auth/google/callback',
   REDIS_HOST: 'localhost',
   REDIS_PASSWORD: '',
@@ -25,8 +25,15 @@ export async function setupGenerator(
 ) {
   // generates the .env file for the database
   generateFiles(tree, path.join(__dirname, 'files'), path.join('.'), {
-    ...LOCAL_DEFAULT_OPTIONS,
-    ...options,
+    DATABASE_URL: options.DATABASE_URL || LOCAL_DEFAULT_OPTIONS.DATABASE_URL,
+    GOOGLE_CLIENT_ID: options.GOOGLE_CLIENT_ID || LOCAL_DEFAULT_OPTIONS.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: options.GOOGLE_CLIENT_SECRET || LOCAL_DEFAULT_OPTIONS.GOOGLE_CLIENT_SECRET,
+    GOOGLE_REDIRECT_URI: options.GOOGLE_REDIRECT_URI || LOCAL_DEFAULT_OPTIONS.GOOGLE_REDIRECT_URI,
+    REDIS_HOST: options.REDIS_HOST || LOCAL_DEFAULT_OPTIONS.REDIS_HOST,
+    REDIS_PASSWORD: options.REDIS_PASSWORD || LOCAL_DEFAULT_OPTIONS.REDIS_PASSWORD,
+    REDIS_PORT: options.REDIS_PORT || LOCAL_DEFAULT_OPTIONS.REDIS_PORT,
+    LETTA_AGENTS_ENDPOINT: options.LETTA_AGENTS_ENDPOINT || LOCAL_DEFAULT_OPTIONS.LETTA_AGENTS_ENDPOINT,
+    NEXT_PUBLIC_CURRENT_HOST: options.NEXT_PUBLIC_CURRENT_HOST || LOCAL_DEFAULT_OPTIONS.NEXT_PUBLIC_CURRENT_HOST,
   });
 
 }
