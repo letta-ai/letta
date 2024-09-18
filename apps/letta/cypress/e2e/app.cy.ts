4d3965cea23353f484819d2d59db2ad994bf7bae
Shubham Naik <shub@memgpt.ai>
2024-09-18 11:02:50 -0700

    chore: fix redirect

--- apps/letta/cypress/e2e/app.cy.ts
+++ apps/letta/cypress/e2e/app.cy.ts
@@ -1,6 +1,11 @@
 describe('letta', () => {
   beforeEach(() => cy.visit('/'));
 
+  it('should be redirected to login page', () => {
+    cy.visit('/');
+    cy.location('pathname').should('eq', '/login');
+  });
+
   it('should log in and visit project home ', () => {
     cy.googleLogin();
 
