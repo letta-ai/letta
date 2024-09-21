62c6c35ca597bfabc5030e06bfb243fe61f039bd
Shubham Naik <shub@memgpt.ai>
2024-09-20 17:09:08 -0700

    chore: add migrations

---
+++ libs/database/migrations/0017_fresh_squadron_sinister.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "organizations" ADD COLUMN "letta_agents_id" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "organizations" ADD CONSTRAINT "organizations_letta_agents_id_unique" UNIQUE("letta_agents_id");
