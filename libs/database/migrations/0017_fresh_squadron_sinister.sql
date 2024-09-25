17f42dd839ceea1db5a7892c28ad597b393ca14a
Shubham Naik <shub@memgpt.ai>
2024-09-25 11:35:44 -0700

    chore: fix db issue

--- libs/database/migrations/0017_fresh_squadron_sinister.sql
+++ libs/database/migrations/0017_fresh_squadron_sinister.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "organizations" ADD COLUMN "letta_agents_id" text NOT NULL;--> statement-breakpoint
 ALTER TABLE "organizations" ADD CONSTRAINT "organizations_letta_agents_id_unique" UNIQUE("letta_agents_id");
+ALTER TABLE "source_agents" DROP CONSTRAINT "source_agents_version_unique" UNIQUE("version");
