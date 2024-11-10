9c78725ac4ddc8315f8597458b1fe4611340830c
Shubham Naik <shub@memgpt.ai>
2024-11-10 07:56:23 -0800

    chore: fix detach timeout

--- apps/letta/cypress/e2e/happy-path.cy.ts
+++ apps/letta/cypress/e2e/happy-path.cy.ts
@@ -66,6 +66,9 @@ describe('letta', () => {
     cy.findByTestId('filetree-actions:1-0').click();
     cy.findByTestId('filetree-action-detach').click();
     cy.findByTestId('detach-data-source-dialog-confirm-button').click();
+    cy.findByTestId('detach-data-source-dialog-confirm-button', {
+      timeout: 10000,
+    }).should('not.be.visible');
     cy.findByTestId('create-data-source-dialog-trigger', {
       timeout: 10000,
     }).click();
