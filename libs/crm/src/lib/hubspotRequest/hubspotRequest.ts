a16749f770150bceee0fdda5dda69e2236570b93
Shubham Naik <shub@memgpt.ai>
2024-12-05 15:16:25 -0800

    chore: prevent hubspot errors

--- libs/crm/src/lib/hubspotRequest/hubspotRequest.ts
+++ libs/crm/src/lib/hubspotRequest/hubspotRequest.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { environment } from '@letta-web/environmental-variables';
 
 export const hubspotRequest = axios.create({
   baseURL: 'https://api.hubapi.com',
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`,
+    Authorization: `Bearer ${environment.HUBSPOT_API_KEY}`,
   },
 });
