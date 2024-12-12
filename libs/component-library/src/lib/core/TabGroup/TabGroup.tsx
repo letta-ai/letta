ce3dea719b97e55d065f5bae13d3c7f6d1963348
Caren Thomas <caren@caren-mac.local>
2024-12-11 17:32:36 -0800

    fix syntax and address design feedback

--- libs/component-library/src/lib/core/TabGroup/TabGroup.tsx
+++ libs/component-library/src/lib/core/TabGroup/TabGroup.tsx
@@ -3,58 +3,48 @@ import React from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
 import { cn } from '@letta-web/core-style-config';
 import { Frame } from '../../framing/Frame/Frame';
+import { Slot } from '@radix-ui/react-slot';
 import { Typography } from '../../core/Typography/Typography';
 
 interface TabGroupItemType {
   label: string;
   value: string;
-  content: string;
+  content: React.ReactNode;
   icon?: React.ReactNode;
 }
 
 interface TabGroupProps extends Tabs.TabsProps {
   items: TabGroupItemType[];
-  border?: boolean;
   fullWidth?: boolean;
 }
 
 export function TabGroup(props: TabGroupProps) {
-  const { items, border, fullWidth, value, defaultValue, onValueChange } =
-    props;
+  const { items, fullWidth, value, defaultValue, onValueChange } = props;
 
   return (
-    <Frame
-      className={cn(
-        border ? 'frame-border-hack' : '',
-        fullWidth ? 'w-full' : ''
-      )}
-    >
+    <Frame className={cn(fullWidth ? 'w-full' : '')}>
       <Tabs.Root
         value={value}
         defaultValue={defaultValue}
         onValueChange={onValueChange}
       >
-        <Tabs.List>
+        <Tabs.List className="mb-2">
           {items.map((item) => (
             <Tabs.Trigger
               className={cn(
-                'px-4 py-2 hover:font-medium border-b-2 border-gray-200 data-[state=active]:font-medium data-[state=active]:text-blue-600 data-[state=active]:border-b-2 data-[state=active]:border-gray-600'
+                'px-4 py-2 hover:font-medium border-b-2 data-[state=active]:font-medium data-[state=active]:border-b-2 data-[state=active]:border-content'
               )}
               value={item.value}
             >
-              {item.icon}
-              <Typography variant="heading6" color="black">
-                {item.label}
-              </Typography>
+              <Slot className="w-4 h-4">{item.icon}</Slot>
+              <Typography variant="body2">{item.label}</Typography>
             </Tabs.Trigger>
           ))}
         </Tabs.List>
 
         {items.map((item) => (
-          <Tabs.Content value={item.value} className="p-4">
-            <Typography variant="body2" align="left">
-              {item.content}
-            </Typography>
+          <Tabs.Content key={item.value} value={item.value}>
+            {item.content}
           </Tabs.Content>
         ))}
       </Tabs.Root>
