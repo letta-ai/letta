df834e47168031f8cd9e6a8b6daa9122d3f7621b
Shubham Naik <shub@memgpt.ai>
2024-12-12 10:17:03 -0800

    chore: add feature flag

--- libs/feature-flags/src/flags.ts
+++ libs/feature-flags/src/flags.ts
@@ -11,23 +11,13 @@ interface FlagProperties {
 // Generate the flag values here:
 // https://app.launchdarkly.com/projects/default/flags?env=test&selected-env=test
 export const featureFlags = {
-  SHOW_CONTEXT_EDITOR: {
-    description: 'Shows the context editor in the ADE',
-    expiryDate: '2024-12-31',
-    flagValue: z.boolean(),
-  },
-  SHOW_PARAMETERS_EDITOR: {
-    description: 'Shows the parameters editor in the ADE',
-    expiryDate: '2024-12-31',
-    flagValue: z.boolean(),
-  },
-  SHOW_VARIABLES_EDITOR: {
-    description: 'Shows the variables editor in the ADE',
-    expiryDate: '2024-12-31',
-    flagValue: z.boolean(),
-  },
   GA_ADE: {
     description: 'General availability of the ADE',
+    expiryDate: '2024-12-31',
+    flagValue: z.boolean(),
+  },
+  SHOW_COMPOSIO_TOOLS: {
+    description: 'Show Composio tools',
     expiryDate: '2024-12-31',
     flagValue: z.boolean(),
   },
