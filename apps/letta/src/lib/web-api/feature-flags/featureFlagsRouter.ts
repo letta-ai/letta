80070e6c2947f815d599510de7f287f3995aaeb1
Shubham Naik <shub@memgpt.ai>
2024-12-19 14:03:28 -0800

    feat: fix composio tools not working for cloud access

--- apps/letta/src/lib/web-api/feature-flags/featureFlagsRouter.ts
+++ apps/letta/src/lib/web-api/feature-flags/featureFlagsRouter.ts
@@ -2,6 +2,7 @@ import type { contracts } from '$letta/web-api/contracts';
 import type { ServerInferResponses } from '@ts-rest/core';
 import { getOrganizationFromOrganizationId, getUser } from '$letta/server/auth';
 import {
+  type FlagMap,
   getDefaultFlags,
   getOrganizationFeatureFlags,
 } from '@letta-web/feature-flags';
@@ -9,6 +10,8 @@ import {
 type FeatureFlagsResponse = ServerInferResponses<
   typeof contracts.featureFlags.getFeatureFlags
 >;
+
+let flagOverrides: Partial<FlagMap> = {};
 
 async function getFeatureFlags(): Promise<FeatureFlagsResponse> {
   const user = await getUser();
@@ -29,9 +32,21 @@ async function getFeatureFlags(): Promise<FeatureFlagsResponse> {
     flags = await getDefaultFlags();
   }
 
+  if (process.env.NODE_ENV !== 'production') {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires
+      flagOverrides = require('../../../../flag.overrides.json');
+    } catch (_e) {
+      //
+    }
+  }
+
   return {
     status: 200,
-    body: flags,
+    body: {
+      ...flags,
+      ...flagOverrides,
+    },
   };
 }
 
