3053a8276d263f85c115fd718ccd3b4a082d221c
Shubham Naik <shub@memgpt.ai>
2024-12-18 17:04:05 -0800

    chore: fix bork

--- apps/letta/src/lib/utils/compareAgentStates/compareAgentStates.ts
+++ apps/letta/src/lib/utils/compareAgentStates/compareAgentStates.ts
@@ -1,34 +1,25 @@
 import type { AgentState } from '@letta-web/letta-agents-api';
-import { isEqual, pick } from 'lodash-es';
+import { isEqual } from 'lodash';
 
 function stateCleaner(state: AgentState) {
   // only compare the following properties
-  const picker = pick(state, [
-    'system',
-    'llm_config',
-    'embedding_config',
-    'memory',
-    'tools',
-    'sources',
-  ]);
-
   return {
-    toolIds: picker.tools
+    toolIds: state.tools
       .map((tool) => tool.id)
       .toSorted((a, b) => (a || '').localeCompare(b || '')),
-    sourceIds: picker.sources
+    sourceIds: state.sources
       .map((source) => source.id)
       .toSorted((a, b) => (a || '').localeCompare(b || '')),
-    llmConfig: picker.llm_config,
-    embedding_config: picker.embedding_config,
-    memoryBlocks: picker.memory.blocks
+    llmConfig: state.llm_config,
+    embedding_config: state.embedding_config,
+    memoryBlocks: state.memory.blocks
       .map((block) => ({
         value: block.value,
         label: block.label,
       }))
       .toSorted((a, b) => (a.label || '').localeCompare(b.label || '')),
-    promptTemplate: picker.memory.prompt_template,
-    system: picker.system,
+    promptTemplate: state.memory.prompt_template,
+    system: state.system,
   };
 }
 
