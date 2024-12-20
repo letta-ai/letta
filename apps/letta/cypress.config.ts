08de95a25f712dce6f95117ee280cdbd258dd6e3
Caren Thomas <caren@mac.lan>
2024-12-19 22:53:07 -0800

    temporarily increase timeout

--- apps/letta/cypress.config.ts
+++ apps/letta/cypress.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   e2e: {
     ...nxE2EPreset(__filename, { cypressDir: 'cypress', bundler: 'vite' }),
     baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 100000,
   },
   env: {
     googleRefreshToken: process.env.CYPRESS_GOOGLE_REFRESH_TOKEN,
