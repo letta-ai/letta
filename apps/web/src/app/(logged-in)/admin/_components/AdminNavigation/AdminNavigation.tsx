d9c9cc9a9161dc6423a24fd4af92e557082ee8c9
Shubham Naik <shub@memgpt.ai>
2024-12-26 15:04:20 -0800

    feat: update nx to latest

--- apps/web/src/app/(logged-in)/admin/_components/AdminNavigation/AdminNavigation.tsx
+++ apps/web/src/app/(logged-in)/admin/_components/AdminNavigation/AdminNavigation.tsx
@@ -28,8 +28,8 @@ function NavigationItem(props: AdminNavigationItemProps) {
 
 export function AdminNavigation() {
   useEffect(() => {
-    document.body.className = 'hacker' || '';
-    document.body.dataset['mode'] = 'hacker' || '';
+    document.body.className = 'hacker';
+    document.body.dataset['mode'] = 'hacker';
   }, []);
 
   return (
