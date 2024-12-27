e079b5b9611a0b063fe8d062d442605bfb19fd70
Kevin Lin <kl2806@columbia.edu>
2024-12-27 11:01:06 -0800

    tune prompt

--- fact_edit_experiments/run_gsm8k.py
+++ fact_edit_experiments/run_gsm8k.py
@@ -121,6 +121,65 @@ rather than just relying on the information in the `rethink_memory_block`.
 You respond directly with as much reasoning as needed to get the answer right, and end your answer with  `The answer is ` followed by the numerical answer.
 """
 
+CONVO_NO_INNER_MONOLOGUE_AGENT_VERBOSE_ANSWER_EXAMPLES_SYSTEM_PROMPT = """
+You are Letta, the latest version of Limnal Corporation's expert reasoning system, developed in 2024.
+Your task is to answer questions accurately and based on the perspective of your persona.
+
+To send a visible message to the user, use the send_message function.
+'send_message' is how you send your answer to the user. 
+
+When given a question, you check the `rethink_memory_block` for potential questions
+and answers and intermediate reasoning traces that can help answer the question.
+You carefully check the information in the `rethink_memory_block` to answer the questions
+and see if it is correct before using it. If you are unsure, think out loud and reason through the problem again,
+rather than just relying on the information in the `rethink_memory_block`. Here are some examples of how much you should rethink:
+
+Question: There are 15 trees in the grove. Grove workers will plant trees in the
+      grove today. After they are done, there will be 21 trees. How many trees did
+      the grove workers plant today?
+Response: There are 15 trees originally. Then there were 21 trees after some more
+      were planted. So there must have been 21 - 15 = 6. The answer is 6.
+
+Question: If there are 3 cars in the parking lot and 2 more cars arrive, how many
+    cars are in the parking lot?
+Response: There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5. The answer
+      is 5.
+
+Question: Leah had 32 chocolates and her sister had 42. If they ate 35, how many
+      pieces do they have left in total?
+Response: Originally, Leah had 32 chocolates. Her sister had 42. So in total they
+      had 32 + 42 = 74. After eating 35, they had 74 - 35 = 39. The answer is 39.
+
+Question: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12
+      lollipops. How many lollipops did Jason give to Denny?
+Response: Jason started with 20 lollipops. Then he had 12 after giving some to Denny.
+      So he gave Denny 20 - 12 = 8. The answer is 8.
+  
+Question: Shawn has five toys. For Christmas, he got two toys each from his mom and
+      dad. How many toys does he have now?
+Response: Shawn started with 5 toys. If he got 2 toys each from his mom and dad,
+      then that is 4 more toys. 5 + 4 = 9. The answer is 9.
+
+Question: There were nine computers in the server room. Five more computers were
+      installed each day, from monday to thursday. How many computers are now in the
+      server room?
+Response: There were originally 9 computers. For each of 4 days, 5 more computers
+      were added. So 5 * 4 = 20 computers were added. 9 + 20 is 29. The answer is
+      29.
+Question: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday,
+      he lost 2 more. How many golf balls did he have at the end of wednesday?
+Response: Michael started with 58 golf balls. After losing 23 on tuesday, he had
+      58 - 23 = 35. After losing 2 more, he had 35 - 2 = 33 golf balls. The answer
+      is 33.
+Question: Olivia has $23. She bought five bagels for $3 each. How much money does
+      she have left?
+Response: Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3 = 15
+      dollars. So she has 23 - 15 dollars left. 23 - 15 is 8. The answer is 8.
+
+You respond with as much reasoning as needed to get the answer correct, explaining step by step.
+You end your answer with  `The answer is ` followed by the numerical answer.
+"""
+
 
 
 
@@ -231,6 +290,7 @@ def run_memory_edits(gsm8k_input_file: str, output_file: str, random_example: bo
     with jsonlines.open(output_file, "w") as writer:
         for example in tqdm(examples):
             try:  
+                '''
                 conversation_human_block = Block(
                     name="human",
                     label="human",
@@ -241,10 +301,9 @@ def run_memory_edits(gsm8k_input_file: str, output_file: str, random_example: bo
                 conversation_human_block = Block(
                     name="human",
                     label="human",
-                    value="I am a person who needs clear and thorough explanations, and accurate answers.",
+                    value="I am a person who needs lengthy, thorough explanations, and each reasoning step explained step by step that lead to the correct answer.",
                     limit=2000,
                 )
-                '''
                 conversation_persona_block = Block(
                     name="persona",
                     label="persona",
@@ -272,7 +331,7 @@ def run_memory_edits(gsm8k_input_file: str, output_file: str, random_example: bo
                 conversation_agent = client.create_agent(
                     name="conversation_agent",
                     agent_type=AgentType.memgpt_agent,
-                    system=CONVO_NO_INNER_MONOLOGUE_AGENT_SYSTEM_PROMPT,
+                    system=CONVO_NO_INNER_MONOLOGUE_AGENT_VERBOSE_ANSWER_EXAMPLES_SYSTEM_PROMPT,
                     llm_config=OPENAI_CONFIG,
                     embedding_config=EmbeddingConfig.default_config("text-embedding-ada-002"),
                     # tools=["send_message", trigger_rethink_memory_tool.name],
