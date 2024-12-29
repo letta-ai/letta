df1fd34301e2c294dc7312bcb31fc877035984bf
Shubham Naik <shub@memgpt.ai>
2024-12-29 00:09:22 -0800

    chore: fix middleware issue

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -4,6 +4,11 @@ import { CURRENT_PATH_HEADER } from '$web/constants';
 
 export function middleware(request: NextRequest) {
   const headers = new Headers(request.headers);
+
   headers.set(CURRENT_PATH_HEADER, request.nextUrl.pathname);
   return NextResponse.next({ headers });
 }
+
+export const config = {
+  matcher: ['/((?!api|v1|_next/static|_next/image|favicon.ico).*)'],
+};
