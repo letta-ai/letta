7b490f54179601adc177cb74ac8f8565dc54cc7c
Kevin Lin <kl2806@columbia.edu>
2025-01-08 11:56:34 -0800

    add token logging for multiple responses

--- evaluate_gsm8k.py
+++ evaluate_gsm8k.py
@@ -27,27 +27,47 @@ def evaluate(input_file: str):
 
 
             final_answer = ""
-            for message in obj['response']['messages']:
-                if message['message_type'] == "function_call":
-                    if message['function_call']['name'] == "send_message":
-                        arguments = json.loads(message['function_call']['arguments'])
-                        final_answer = arguments['message']
 
-            # TODO: make sure this matches lm-evaluation-harness
-            regex_str = "(-?[$0-9.,]{2,})|(-?[0-9]+)"
-            matches = re.findall(regex_str, final_answer)
+            # do majority voting over obj['responses']
 
-            if matches == []:
-                continue
-            final_num = "".join(matches[-1])
-            final_num = final_num.replace("$", "")
-            final_num = final_num.replace(",", "")
-            final_num = final_num.strip(".")
 
-            if final_num == answer: # TODO: should we convert to float
+            if 'responses' not in obj:
+                # then just do 'response'
+                for message in obj['response']['messages']:
+                    if message['message_type'] == "function_call":
+                        if message['function_call']['name'] == "send_message":
+                            arguments = json.loads(message['function_call']['arguments'])
+                            final_answer = arguments['message']
+                            break
+            votes = {}
+            for response in obj['responses']:
+                for message in response['messages']:
+                    if message['message_type'] == "function_call":
+                        if message['function_call']['name'] == "send_message":
+                            arguments = json.loads(message['function_call']['arguments'])
+                            final_answer = arguments['message']
+
+                            # TODO: make sure this matches lm-evaluation-harness
+                            regex_str = "(-?[$0-9.,]{2,})|(-?[0-9]+)"
+                            matches = re.findall(regex_str, final_answer)
+
+                            if matches == []:
+                                continue
+                            final_answer = "".join(matches[-1])
+                            final_answer = final_answer.replace("$", "")
+                            final_answer = final_answer.replace(",", "")
+                            final_answer = final_answer.strip(".")
+
+                            if final_answer in votes:
+                                votes[final_answer] += 1
+                            else:
+                                votes[final_answer] = 1
+
+            final_answer = max(votes, key=votes.get)
+            if final_answer == answer: # TODO: should we convert to float
                 correct += 1
             else:
-                print("Final Answer: ", final_num)
+                print("Final Answer: ", final_answer)
                 print("Expected Answer: ", answer)
                 print("Matches: ", matches)
                 print("\n\n")
@@ -65,18 +85,34 @@ def evaluate(input_file: str):
     with jsonlines.open(input_file) as reader:
         lines = list(reader)
     print("avg. num rethinks: ", statistics.mean(example_num_rethinks))
-    print("avg. completion tokens: ", statistics.mean([obj['response']['usage']['completion_tokens'] for obj in lines]))
-    print("std. completion tokens: ", statistics.stdev([obj['response']['usage']['completion_tokens'] for obj in lines]))
-    print("avg. prompt tokens: ", statistics.mean([obj['response']['usage']['prompt_tokens'] for obj in lines]))
-    print("std. prompt tokens: ", statistics.stdev([obj['response']['usage']['prompt_tokens'] for obj in lines]))
 
-    # write avg completion tokens and accuracy out the token count information to a file with the same prefix as the input file in json
-    with open(input_file.replace(".jsonl", "_results.json"), "w") as f:
-        json.dump({"avg_completion_tokens": statistics.mean([obj['response']['usage']['completion_tokens'] for obj in lines]),
-                   "std_completion_tokens": statistics.stdev([obj['response']['usage']['completion_tokens'] for obj in lines]),
-                   "avg_prompt_tokens": statistics.mean([obj['response']['usage']['prompt_tokens'] for obj in lines]),
-                   "std_prompt_tokens": statistics.stdev([obj['response']['usage']['prompt_tokens'] for obj in lines]),
-                     "accuracy": correct / total}, f)
+    if 'response' in obj:
+        print("avg. completion tokens: ", statistics.mean([obj['response']['usage']['completion_tokens'] for obj in lines]))
+        print("std. completion tokens: ", statistics.stdev([obj['response']['usage']['completion_tokens'] for obj in lines]))
+        print("avg. prompt tokens: ", statistics.mean([obj['response']['usage']['prompt_tokens'] for obj in lines]))
+        print("std. prompt tokens: ", statistics.stdev([obj['response']['usage']['prompt_tokens'] for obj in lines]))
+
+        # write avg completion tokens and accuracy out the token count information to a file with the same prefix as the input file in json
+        with open(input_file.replace(".jsonl", "_results.json"), "w") as f:
+            json.dump({"avg_completion_tokens": statistics.mean([obj['response']['usage']['completion_tokens'] for obj in lines]),
+                    "std_completion_tokens": statistics.stdev([obj['response']['usage']['completion_tokens'] for obj in lines]),
+                    "avg_prompt_tokens": statistics.mean([obj['response']['usage']['prompt_tokens'] for obj in lines]),
+                    "std_prompt_tokens": statistics.stdev([obj['response']['usage']['prompt_tokens'] for obj in lines]),
+                        "accuracy": correct / total}, f)
+    if 'responses' in obj:
+        # sum over all responses
+        print("avg. completion tokens: ", statistics.mean([sum([response['usage']['completion_tokens'] for response in obj['responses']]) for obj in lines]))
+        print("std. completion tokens: ", statistics.stdev([sum([response['usage']['completion_tokens'] for response in obj['responses']]) for obj in lines]))
+        print("avg. prompt tokens: ", statistics.mean([sum([response['usage']['prompt_tokens'] for response in obj['responses']]) for obj in lines]))
+        print("std. prompt tokens: ", statistics.stdev([sum([response['usage']['prompt_tokens'] for response in obj['responses']]) for obj in lines]))
+
+        # write avg completion tokens and accuracy out the token count information to a file with the same prefix as the input file in json
+        with open(input_file.replace(".jsonl", "_results.json"), "w") as f:
+            json.dump({"avg_completion_tokens": statistics.mean([sum([response['usage']['completion_tokens'] for response in obj['responses']]) for obj in lines]),
+                    "std_completion_tokens": statistics.stdev([sum([response['usage']['completion_tokens'] for response in obj['responses']]) for obj in lines]),
+                    "avg_prompt_tokens": statistics.mean([sum([response['usage']['prompt_tokens'] for response in obj['responses']]) for obj in lines]),
+                    "std_prompt_tokens": statistics.stdev([sum([response['usage']['prompt_tokens'] for response in obj['responses']]) for obj in lines]),
+                    "accuracy": correct / total}, f)
 
 
 
