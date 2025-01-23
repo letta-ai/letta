4f555e7f644e31159d3229506a750525677785fe
Shubham Naik <shubham.naik10@gmail.com>
2025-01-22 21:25:05 -0800

    Windows support (#740)

--- apps/desktop-electron/scripts/before-build.js
+++ apps/desktop-electron/scripts/before-build.js
@@ -1,9 +1,10 @@
 // check os
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 
-module.exports = async ({ arch }) => {
-  if (arch === 'win32') {
+module.exports = async () => {
+  if (os.platform() === 'win32') {
     // remove `letta` from `dist` folder
     const distPath = path.resolve(__dirname, '../dist');
     const lettaPath = path.resolve(distPath, 'letta');
