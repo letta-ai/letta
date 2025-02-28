53a1d7ae63ef02a5538c3a74a07703345f4eb383
Krishnakumar R (KK) <65895020+kk-src@users.noreply.github.com>
2025-02-28 10:42:37 +0000

    refactor(azure-embedding): Removed unnecessary and commented Azure embedding code
    Removed the embedding calls in `azure_openai.py` which is no
    longer used. The code path exercised is the `get_text_embedding` in
    `class AzureOpenAIEmbedding`. Tested this by running directory
    upload with Azure embedding model.
    
    Also removed commented out code from Azure code path as a general
    clean up.

--- letta/llm_api/azure_openai.py
+++ letta/llm_api/azure_openai.py
@@ -126,15 +126,3 @@ def azure_openai_chat_completions_request(
         response_json["choices"][0]["message"]["content"] = None
     response = ChatCompletionResponse(**response_json)  # convert to 'dot-dict' style which is the openai python client default
     return response
-
-
-def azure_openai_embeddings_request(
-    resource_name: str, deployment_id: str, api_version: str, api_key: str, data: dict
-) -> EmbeddingResponse:
-    """https://learn.microsoft.com/en-us/azure/ai-services/openai/reference#embeddings"""
-
-    url = f"https://{resource_name}.openai.azure.com/openai/deployments/{deployment_id}/embeddings?api-version={api_version}"
-    headers = {"Content-Type": "application/json", "api-key": f"{api_key}"}
-
-    response_json = make_post_request(url, headers, data)
-    return EmbeddingResponse(**response_json)
