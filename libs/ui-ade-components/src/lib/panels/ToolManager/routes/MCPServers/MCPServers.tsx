b98714acad038c77101dfb752c9913e02ec6c67b
Shubham Naik <shub@letta.com>
2025-03-27 10:36:31 -0800

    chore: mcp server block (#1428)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- libs/ui-ade-components/src/lib/panels/ToolManager/routes/MCPServers/MCPServers.tsx
+++ libs/ui-ade-components/src/lib/panels/ToolManager/routes/MCPServers/MCPServers.tsx
@@ -449,6 +449,8 @@ function MCPServerList(props: MCPServerListProps) {
 export function MCPServers() {
   const t = useTranslations('ToolsEditor/MCPServers');
 
+  const { isLocal } = useCurrentAgentMetaData();
+
   const [search, setSearch] = React.useState('');
 
   const { data: servers, isLoading, isError } = useToolsServiceListMcpServers();
@@ -483,6 +485,14 @@ export function MCPServers() {
       [search],
     );
   }, [search, serversAsArray]);
+
+  if (!isLocal) {
+    return (
+      <VStack fullWidth fullHeight align="center" justify="center">
+        <LoadingEmptyStatusComponent emptyMessage={t('notLocal')} />
+      </VStack>
+    );
+  }
 
   return (
     <ToolManagerPage border>
