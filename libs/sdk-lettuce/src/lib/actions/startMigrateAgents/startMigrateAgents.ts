6d9a88490d27494cc8e697ff81db9b1771cc4baa
Shubham Naik <shub@letta.com>
2025-04-01 10:24:40 -0800

    chore: fix workflow name (#1511)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- libs/sdk-lettuce/src/lib/actions/startMigrateAgents/startMigrateAgents.ts
+++ libs/sdk-lettuce/src/lib/actions/startMigrateAgents/startMigrateAgents.ts
@@ -1,13 +1,12 @@
 import { getTemporalClient } from '../../../index';
 import { TASK_QUEUE_NAME } from '../../config';
-import { migrateAgentsWorkflow } from '../../workflows/migrateAgentsWorkflow/migrateAgentsWorkflow';
 import type { MigrateAgentsPayload } from '../../types';
 import crypto from 'crypto';
 
 export async function startMigrateAgents(payload: MigrateAgentsPayload) {
   const sessionId = crypto.randomUUID();
 
-  await getTemporalClient().workflow.start(migrateAgentsWorkflow, {
+  await getTemporalClient().workflow.start('migrateAgentsWorkflow', {
     taskQueue: TASK_QUEUE_NAME,
     args: [payload],
     workflowId: 'migrate-agents-workflow-' + sessionId,
