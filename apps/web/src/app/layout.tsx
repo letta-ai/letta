ca92615ab94d1dbcd927df5be772cfd38b03a9da
Shubham Naik <shub@letta.com>
2025-04-10 17:11:13 -0700

    fix logged out icos (#1674)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -41,84 +41,19 @@ export default async function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/seo/apple-icon-57x57.png"
+          rel="icon"
+          type="image/png"
+          href="/seo/favicon-96x96.png"
+          sizes="96x96"
         />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/seo/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/seo/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/seo/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/seo/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/seo/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/seo/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/seo/apple-icon-152x152.png"
-        />
+        <link rel="icon" type="image/svg+xml" href="/seo/favicon.svg" />
+        <link rel="shortcut icon" href="/seo/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/seo/apple-icon-180x180.png"
+          href="/seo/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/seo/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/seo/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/seo/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/seo/favicon-16x16.png"
-        />
-        <link
-          rel="icon"
-          id="favicon"
-          href="/icon?<generated>"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="manifest" href="/seo/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <link rel="manifest" href="/seo/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
         <title>Letta</title>
       </head>
