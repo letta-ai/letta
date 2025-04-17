e19a2e2b3d6070918a31b12a281af803f1e80519
Shubham Naik <shub@letta.com>
2025-04-16 22:02:01 -0700

    1mt (#1751)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- libs/sdk-lettuce/src/lib/workflows/migrateAgentsWorkflow/migrateAgentsWorkflow.ts
+++ libs/sdk-lettuce/src/lib/workflows/migrateAgentsWorkflow/migrateAgentsWorkflow.ts
@@ -2,7 +2,9 @@ import { proxyActivities } from '@temporalio/workflow';
 import type { activities } from '../../activities';
 import type { MigrateAgentsPayload } from '../../types';
 
-const { migrateAgents } = proxyActivities<typeof activities>({});
+const { migrateAgents } = proxyActivities<typeof activities>({
+  startToCloseTimeout: '1 hour',
+});
 
 export async function migrateAgentsWorkflow(
   payload: MigrateAgentsPayload,
