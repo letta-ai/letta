b275a015449ffbb3f87865078273b993d0c86c55
Shubham Naik <shub@letta.com>
2025-04-16 21:42:15 -0700

    Continuously deploy lettuce (#1749)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- libs/sdk-lettuce/src/lib/workflows/migrateAgentsWorkflow/migrateAgentsWorkflow.ts
+++ libs/sdk-lettuce/src/lib/workflows/migrateAgentsWorkflow/migrateAgentsWorkflow.ts
@@ -2,9 +2,7 @@ import { proxyActivities } from '@temporalio/workflow';
 import type { activities } from '../../activities';
 import type { MigrateAgentsPayload } from '../../types';
 
-const { migrateAgents } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '5 seconds',
-});
+const { migrateAgents } = proxyActivities<typeof activities>({});
 
 export async function migrateAgentsWorkflow(
   payload: MigrateAgentsPayload,
