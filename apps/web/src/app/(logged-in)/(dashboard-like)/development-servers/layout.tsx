39bb780268b0cebe861a9b73782a55ea2d403fe3
Shelley Pham <shelleypham@users.noreply.github.com>
2025-05-06 13:13:05 -0700

    chore: remove feature flag `NEW_DEVELOPMENT_SERVER` (#2025)

--- apps/web/src/app/(logged-in)/(dashboard-like)/development-servers/layout.tsx
+++ apps/web/src/app/(logged-in)/(dashboard-like)/development-servers/layout.tsx
@@ -5,137 +5,26 @@ import {
   PlusIcon,
   LettaInvaderOutlineIcon,
   HStack,
-  Typography,
   SpaceDashboardIcon,
   StatusIndicator,
-  Button,
-  DotsHorizontalIcon,
-  DropdownMenuItem,
-  CogIcon,
-  TrashIcon,
-  DropdownMenu,
-  Dialog,
   ServerNavigationDropdown,
   IdentitiesIcon,
   HR,
 } from '@letta-cloud/ui-component-library';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useTranslations } from '@letta-cloud/translations';
 import { webApi, webApiQueryKeys } from '$web/client';
 import {
   LOCAL_PROJECT_SERVER_URL,
   SUPPORTED_LETTA_AGENTS_VERSIONS,
 } from '$web/constants';
-import { UpdateDevelopmentServerDetailsDialog } from './shared/UpdateDevelopmentServerDetailsDialog/UpdateDevelopmentServerDetailsDialog';
-import { useQueryClient } from '@tanstack/react-query';
 import { useParams, useRouter } from 'next/navigation';
-import type { ServerInferResponses } from '@ts-rest/core';
-import {
-  useFeatureFlag,
-  type developmentServersContracts,
-} from '$web/web-api/contracts';
 import { useDevelopmentServerStatus } from '$web/client/hooks/useDevelopmentServerStatus/useDevelopmentServerStatus';
 import semver from 'semver/preload';
 import type { DevelopmentServerConfig } from '@letta-cloud/utils-client';
 
-interface DeleteDevelopmentServerDialogProps {
-  trigger: React.ReactNode;
-  id: string;
-}
-
-function DeleteDevelopmentServerDialog(
-  props: DeleteDevelopmentServerDialogProps,
-) {
-  const t = useTranslations('development-servers/layout');
-  const { trigger, id } = props;
-  const queryClient = useQueryClient();
-  const { push } = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
-
-  const { mutate, isPending, isSuccess, isError } =
-    webApi.developmentServers.deleteDevelopmentServer.useMutation({
-      onSuccess: () => {
-        queryClient.setQueriesData<
-          ServerInferResponses<
-            typeof developmentServersContracts.getDevelopmentServers,
-            200
-          >
-        >(
-          {
-            queryKey: webApiQueryKeys.developmentServers.getDevelopmentServers,
-            exact: true,
-          },
-          (oldData) => {
-            if (!oldData) {
-              return oldData;
-            }
-
-            return {
-              ...oldData,
-              body: {
-                ...oldData.body,
-                developmentServers: oldData.body.developmentServers.filter(
-                  (server) => server.id !== id,
-                ),
-              },
-            };
-          },
-        );
-
-        push('/development-servers/local/dashboard');
-      },
-    });
-
-  const handleDelete = useCallback(() => {
-    if (isPending || isSuccess) {
-      return;
-    }
-
-    mutate({
-      params: {
-        developmentServerId: id,
-      },
-    });
-  }, [isPending, isSuccess, mutate, id]);
-
-  return (
-    <Dialog
-      trigger={trigger}
-      isConfirmBusy={isPending || isSuccess}
-      onConfirm={handleDelete}
-      errorMessage={
-        isError ? t('DeleteDevelopmentServerDialog.error') : undefined
-      }
-      title={t('DeleteDevelopmentServerDialog.title')}
-      confirmText={t('DeleteDevelopmentServerDialog.confirm')}
-      isOpen={isOpen}
-      onOpenChange={setIsOpen}
-    >
-      {t('DeleteDevelopmentServerDialog.description')}
-    </Dialog>
-  );
-}
-
 interface LocalProjectLayoutProps {
   children: React.ReactNode;
-}
-
-interface ServerStatusTitleProps {
-  config: DevelopmentServerConfig;
-  title: string;
-}
-
-function ServerStatusTitle(props: ServerStatusTitleProps) {
-  const { config, title } = props;
-
-  return (
-    <HStack align="center">
-      <ServerStatusIndicator config={config} />
-      <Typography bold variant="body2">
-        {title}
-      </Typography>
-    </HStack>
-  );
 }
 
 interface ServerStatusIndicatorProps {
@@ -190,11 +79,11 @@ const localServerConfig: DevelopmentServerConfig = {
   password: '',
 };
 
-interface NewDashboardLayoutProps {
+interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
-function NewDashboardLayout(props: NewDashboardLayoutProps) {
+function DashboardLayout(props: DashboardLayoutProps) {
   const { children } = props;
 
   const { data: remoteConnections } =
@@ -291,146 +180,9 @@ function NewDashboardLayout(props: NewDashboardLayoutProps) {
 }
 
 function LocalProjectLayout(props: LocalProjectLayoutProps) {
-  const t = useTranslations('development-servers/layout');
   const { children } = props;
 
-  const { data: remoteConnections } =
-    webApi.developmentServers.getDevelopmentServers.useQuery({
-      queryKey: webApiQueryKeys.developmentServers.getDevelopmentServers,
-    });
-
-  const additionalNavigationItems = useMemo(() => {
-    if (!remoteConnections) {
-      return [];
-    }
-
-    return remoteConnections.body.developmentServers.map((server) => ({
-      title: server.name,
-      titleOverride: (
-        <HStack justify="spaceBetween" fullWidth>
-          <ServerStatusTitle
-            config={{
-              password: server.password || '',
-              url: server.url,
-              id: server.id,
-              name: server.name,
-            }}
-            title={server.name}
-          />
-          <DropdownMenu
-            triggerAsChild
-            trigger={
-              <Button
-                hideLabel
-                color="tertiary"
-                size="small"
-                preIcon={<DotsHorizontalIcon />}
-                label={t('settings')}
-              />
-            }
-          >
-            <UpdateDevelopmentServerDetailsDialog
-              trigger={
-                <DropdownMenuItem
-                  preIcon={<CogIcon />}
-                  doNotCloseOnSelect
-                  label={t('updateDetails')}
-                />
-              }
-              name={server.name}
-              password={server.password || ''}
-              url={server.url}
-              id={server.id}
-            />
-            <DeleteDevelopmentServerDialog
-              trigger={
-                <DropdownMenuItem
-                  preIcon={<TrashIcon />}
-                  doNotCloseOnSelect
-                  label={t('delete')}
-                />
-              }
-              id={server.id}
-            />
-          </DropdownMenu>
-        </HStack>
-      ),
-      items: [
-        {
-          id: server.id,
-          icon: <LettaInvaderOutlineIcon />,
-          label: t('nav.agents'),
-          href: `/development-servers/${server.id}/agents`,
-        },
-        {
-          id: 'identities',
-          icon: <IdentitiesIcon />,
-          label: t('nav.identities'),
-          href: `/development-servers/${server.id}/identities`,
-        },
-      ],
-    }));
-  }, [remoteConnections, t]);
-
-  const { isLoading: isLoadingFlag, data: isFlagEnabled } = useFeatureFlag(
-    'NEW_DEVELOPMENT_SERVER',
-  );
-
-  if (!isLoadingFlag && isFlagEnabled) {
-    return <NewDashboardLayout>{children}</NewDashboardLayout>;
-  }
-
-  return (
-    <DashboardWithSidebarWrapper
-      baseUrl="/development-servers"
-      returnOverride="/"
-      navigationItems={[
-        {
-          title: t('nav.local'),
-          titleOverride: (
-            <ServerStatusTitle
-              config={{
-                id: 'local',
-                url: LOCAL_PROJECT_SERVER_URL,
-                password: '',
-                name: t('nav.local'),
-              }}
-              title={t('nav.local')}
-            />
-          ),
-          items: [
-            {
-              id: 'dashboard',
-              icon: <SpaceDashboardIcon />,
-              label: t('nav.dashboard'),
-              href: `/development-servers/local/dashboard`,
-            },
-            {
-              id: 'agents',
-              icon: <LettaInvaderOutlineIcon />,
-              label: t('nav.agents'),
-              href: `/development-servers/local/agents`,
-            },
-            {
-              id: 'identities',
-              icon: <IdentitiesIcon />,
-              label: t('nav.identities'),
-              href: `/development-servers/local/identities`,
-            },
-          ],
-        },
-        ...additionalNavigationItems,
-        {
-          id: 'create',
-          icon: <PlusIcon />,
-          label: t('nav.create'),
-          href: '/development-servers/add',
-        },
-      ]}
-    >
-      {children}
-    </DashboardWithSidebarWrapper>
-  );
+  return <DashboardLayout>{children}</DashboardLayout>;
 }
 
 export default LocalProjectLayout;
