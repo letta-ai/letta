4c9321e04e35bcbb86c837b49f7e0b4a7ac8fe25
Shubham Naik <shub@letta.com>
2025-06-06 15:04:24 -0700

    Shub/let 2178 create a new icon for templates (#2687)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- libs/ui-component-library/src/lib/icons/_autogenerated/TemplateIcon.tsx
+++ libs/ui-component-library/src/lib/icons/_autogenerated/TemplateIcon.tsx
@@ -6,16 +6,17 @@ export function TemplateIcon(props: IconWrappedProps) {
   return (
     <IconWrapper {...props}>
       <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M4.91667 14.5V2H17.4167V14.5H4.91667ZM6.16667 13.25H16.1667V6.18271H10.654V3.25H6.16667V13.25ZM2 17.4167V5.17313H3.25V16.1667H14.2435V17.4167H2Z"
+          d="M22 22H5V5H22V22ZM9.5 17.5V19H11V17.5H9.5ZM16 17.5V19H17.5V17.5H16ZM11 9V11H9V16H11V17.5H12.5V16H14.5V17.5H16V16H18V11H16V9H14.5V10H12.5V9H11ZM12.5 13V15H11V13H12.5ZM16 13V15H14.5V13H16Z"
           fill="currentColor"
         />
+        <path d="M20 3.5H18.5H3.5V18.5V20H2V2H20V3.5Z" fill="currentColor" />
       </svg>
     </IconWrapper>
   );
