21e9b6acb814d8b03dea06de058a021d79678bb8
Shubham Naik <shub@letta.com>
2025-06-10 11:52:32 -0700

    chore: system alert fix (#2728)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- apps/web/src/app/_components/SystemAlert/SystemAlert.tsx
+++ apps/web/src/app/_components/SystemAlert/SystemAlert.tsx
@@ -10,9 +10,13 @@ export function SystemAlert() {
   }
 
   return (
-    <div className="system-alert gap-2 border-b border-warning bg-background-warning text-background-warning-content flex items-center px-2 font-bold text-sm">
-      <WarningIcon />
-      {systemWarning.title}
-    </div>
+    <>
+      <div className="system-alert" />
+      <div className="system-alert z-10 overflow-auto  top-0 w-full fixed gap-2 border-b border-warning bg-background-warning text-background-warning-content flex  px-2 font-bold text-sm">
+        {/* eslint-disable-next-line react/forbid-component-props */}
+        <WarningIcon className="min-w-4" />
+        <div className="pt-3">{systemWarning.title}</div>
+      </div>
+    </>
   );
 }
