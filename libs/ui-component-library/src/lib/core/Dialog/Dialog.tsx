82479e9e33d28f42b5db1fb405877f6cabd42138
Shubham Naik <shub@letta.com>
2025-06-23 16:09:50 -0400

    feat: fix safari modals (#2969)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- libs/ui-component-library/src/lib/core/Dialog/Dialog.tsx
+++ libs/ui-component-library/src/lib/core/Dialog/Dialog.tsx
@@ -89,43 +89,45 @@ const DialogContent = React.forwardRef<
         <DialogOverlay />
         <DialogPrimitive.Content ref={ref} {...props}>
           <div id="dialog-dropdown-content" className="z-dropdown" />
-          <div
-            className={cn(
-              'fixed flex flex-col max-h-[95dvh] text-base left-[50%] top-[50%] z-dialog w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-2 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
-              color === 'background' ? 'bg-background' : 'bg-background-grey',
-              className,
-            )}
-          >
-            <VStack
-              className="max-h-[100%]"
-              overflow="hidden"
-              flex
-              gap={false}
-              fullHeight={isFull}
-            >
-              {errorMessage && (
-                <Alert
-                  fullWidth
-                  title={errorMessage}
-                  children={errorAdditionalMessage}
-                  variant="destructive"
-                />
+          <div className="fixed top-0 left-0  w-[100dvw] flex items-center justify-center h-[100dvh] z-dialog pointer-events-none">
+            <div
+              className={cn(
+                'flex flex-col max-h-[95dvh] overflow-y-auto text-base pointer-events-auto w-full max-w-lg  gap-2 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+                color === 'background' ? 'bg-background' : 'bg-background-grey',
+                className,
               )}
+            >
               <VStack
-                flex
                 className="max-h-[100%]"
                 overflow="hidden"
+                flex
                 gap={false}
-                position="relative"
                 fullHeight={isFull}
               >
-                {children}
-                <DialogPrimitive.Close className="absolute right-4 top-[13px] opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-                  <CloseIcon className="h-5 w-5" />
-                  <span className="sr-only">Close</span>
-                </DialogPrimitive.Close>
+                {errorMessage && (
+                  <Alert
+                    fullWidth
+                    title={errorMessage}
+                    children={errorAdditionalMessage}
+                    variant="destructive"
+                  />
+                )}
+                <VStack
+                  flex
+                  className="max-h-[100%]"
+                  overflow="hidden"
+                  gap={false}
+                  position="relative"
+                  fullHeight={isFull}
+                >
+                  {children}
+                  <DialogPrimitive.Close className="absolute right-4 top-[13px] opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+                    <CloseIcon className="h-5 w-5" />
+                    <span className="sr-only">Close</span>
+                  </DialogPrimitive.Close>
+                </VStack>
               </VStack>
-            </VStack>
+            </div>
           </div>
         </DialogPrimitive.Content>
       </DialogPortal>
@@ -150,6 +152,7 @@ function DialogHeader({
     />
   );
 }
+
 DialogHeader.displayName = 'DialogHeader';
 
 function DialogFooter({
@@ -169,6 +172,7 @@ function DialogFooter({
     />
   );
 }
+
 DialogFooter.displayName = 'DialogFooter';
 
 const DialogTitle = React.forwardRef<
--- libs/ui-component-library/src/lib/onboarding/OnboardingPrimaryDialog/OnboardingPrimaryDialog.tsx
+++ libs/ui-component-library/src/lib/onboarding/OnboardingPrimaryDialog/OnboardingPrimaryDialog.tsx
@@ -44,17 +44,19 @@ export function OnboardingPrimaryDialog(props: OnboardingPrimaryDialogProps) {
   return (
     <DialogPrimitive.Root open={isOpen}>
       <DialogOverlay />
-      <DialogPrimitive.Content className="fixed flex flex-col max-w-[468px]  text-base left-[50%] top-[50%] z-dialog w-full bg-background gap-0 translate-x-[-50%] translate-y-[-50%] shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]">
-        <Image width={468} height={250} src={imageUrl} alt="" />
-        <VStack gap="xlarge" padding color="background-grey">
-          <DialogTitle className="sr-only">{title}</DialogTitle>
-          <VStack>{children}</VStack>
-          <HStack align="center" justify="spaceBetween">
-            {secondaryAction || <div />}
-            {primaryAction}
-          </HStack>
-        </VStack>
-      </DialogPrimitive.Content>
+      <div className="fixed w-[100dvw] flex items-center justify-center h-[100dvh] z-dialog pointer-events-none">
+        <DialogPrimitive.Content className="max-h-[100dvh] overflow-y-auto flex flex-col max-w-[468px] pointer-events-auto text-base  w-full bg-background gap-0   shadow-lg duration-200 ">
+          <Image width={468} height={250} src={imageUrl} alt="" />
+          <VStack gap="xlarge" padding color="background-grey">
+            <DialogTitle className="sr-only">{title}</DialogTitle>
+            <VStack>{children}</VStack>
+            <HStack align="center" justify="spaceBetween">
+              {secondaryAction || <div />}
+              {primaryAction}
+            </HStack>
+          </VStack>
+        </DialogPrimitive.Content>
+      </div>
     </DialogPrimitive.Root>
   );
 }
