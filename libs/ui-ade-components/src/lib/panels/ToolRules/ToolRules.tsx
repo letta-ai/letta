d821ca5c210baa57a3c38066fc5ad491c5693946
Andy Li <55300002+cliandy@users.noreply.github.com>
2025-06-27 16:18:22 -0700

    chore: remove tool rules editor flag (#3083)

--- libs/ui-ade-components/src/lib/panels/ToolRules/ToolRules.tsx
+++ libs/ui-ade-components/src/lib/panels/ToolRules/ToolRules.tsx
@@ -2,13 +2,9 @@ import { HStack, VStack } from '@letta-cloud/ui-component-library';
 import { useCurrentAgent } from '../../hooks';
 import { ToolRulesList } from './ToolRulesList';
 import { ToolRulesVisual } from './ToolRulesVisual';
-import { useFeatureFlag } from '@letta-cloud/sdk-web';
 
 export function ToolRulesEditor() {
   const agent = useCurrentAgent();
-  const { data: splitEnabled } = useFeatureFlag('TOOL_RULE_VIEWER');
-
-  if (splitEnabled) {
     return (
       <HStack gap={false} fullHeight fullWidth>
         <VStack fullHeight flex>
@@ -19,7 +15,4 @@ export function ToolRulesEditor() {
         </VStack>
       </HStack>
     );
-  }
-
-  return <ToolRulesList defaultToolRules={agent?.tool_rules} />;
 }
