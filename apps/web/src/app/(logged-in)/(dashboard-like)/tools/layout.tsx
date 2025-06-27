c0e73f5ad777b8a15890c12028b8373edda6e636
Shubham Naik <shub@letta.com>
2025-06-27 10:17:38 -0700

    chore: fix return override (#3059)
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- apps/web/src/app/(logged-in)/(dashboard-like)/tools/layout.tsx
+++ apps/web/src/app/(logged-in)/(dashboard-like)/tools/layout.tsx
@@ -24,6 +24,7 @@ export default function ToolsLayout(props: ToolsLayoutProps) {
   return (
     <DashboardWithSidebarWrapper
       baseUrl="/tools"
+      returnOverride="/"
       returnText={t('nav.return')}
       navigationItems={[
         {
