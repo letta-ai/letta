b23bf746bdb727ed9a0b8627f920ee159f766462
Matthew Zhou <mattzh1314@gmail.com>
2025-06-29 22:45:39 -0700

    feat: Add back multi agent tools and protections for cloud (#3100)

--- libs/sdk-core/src/lib/_autogenerated/requests/core/OpenAPI.ts
+++ libs/sdk-core/src/lib/_autogenerated/requests/core/OpenAPI.ts
@@ -47,7 +47,7 @@ export const OpenAPI: OpenAPIConfig = {
   PASSWORD: undefined,
   TOKEN: undefined,
   USERNAME: undefined,
-  VERSION: '0.8.7',
+  VERSION: '0.8.8',
   WITH_CREDENTIALS: false,
   interceptors: {
     request: new Interceptors(),
