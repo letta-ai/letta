4b9e722e90d3f2e094048c448b21dbfb1aa4d69a
Shelley Pham <shelleypham@users.noreply.github.com>
2025-07-15 13:55:24 -0700

    chore: remove unused analytic events (#3335)

--- libs/service-analytics/src/events.ts
+++ libs/service-analytics/src/events.ts
@@ -7,22 +7,12 @@ export enum AnalyticsEvent {
   LOCAL_AGENT_VISITED = 'Local Agent Visited',
   LOCAL_AGENT_MESSAGE_CREATED = 'Local Agent Message Created',
   LOCAL_AGENT_DATA_SOURCE_ATTACHED = 'Local Agent Data Source Attached',
-  LOCAL_AGENT_DATA_SOURCE_DETACHED = 'Local Agent Data Source Detached',
-  LOCAL_AGENT_MODEL_CHANGED = 'Local Agent Model Changed',
   LOCAL_AGENT_DELETED = 'Local Agent Deleted',
   CLOUD_DATA_SOURCE_ATTACHED = 'Cloud Data Source Attached',
-  CLOUD_DATA_SOURCE_DETACHED = 'Cloud Data Source Detached',
-  CLOUD_AGENT_CREATED = 'Cloud Agent Created',
-  CLOUD_AGENT_VISITED = 'Cloud Agent Visited',
   CLOUD_AGENT_MESSAGE_CREATED = 'Cloud Agent Message Created',
-  CLOUD_AGENT_MODEL_CHANGED = 'Cloud Agent Model Changed',
   CLOUD_AGENT_DELETED = 'Cloud Agent Deleted',
   CLOUD_AGENT_MESSAGE_CREATED_IN_API = 'API Cloud Agent Message Created',
   SUBSCRIPTION_CHANGED = 'Subscription Changed',
-  AGENT_CREATED = 'Agent Created',
-  AGENT_STAGED = 'Agent Staged',
-  AGENT_DEPLOYED = 'Agent Deployed',
-  APP_ERROR = 'App Error',
   CREATED_PROJECT = 'Created Project',
   CREATED_TEMPLATE = 'Created Template',
   ANSWERED_ONBOARDING_SURVEY = 'Answered Onboarding Survey',
@@ -41,10 +31,6 @@ export interface BaseProperty {
 
 interface LocalAgentCreatedProperty extends BaseProperty {
   starterKitId: string;
-}
-
-interface LocalAgentModelChangedProperty extends BaseProperty {
-  model: string;
 }
 
 interface AnsweredOnboardingSurveyProperty extends BaseProperty {
@@ -71,26 +57,16 @@ interface SubscriptionChangedProperty {
 }
 
 export interface AnalyticsEventProperties {
-  [AnalyticsEvent.AGENT_CREATED]: BaseProperty;
   [AnalyticsEvent.USER_LOGGED_IN]: BaseProperty;
-  [AnalyticsEvent.AGENT_STAGED]: BaseProperty;
-  [AnalyticsEvent.AGENT_DEPLOYED]: BaseProperty;
   [AnalyticsEvent.USER_CREATED]: BaseProperty;
   [AnalyticsEvent.LOCAL_AGENT_CREATED]: LocalAgentCreatedProperty;
   [AnalyticsEvent.LOCAL_AGENT_VISITED]: BaseProperty;
   [AnalyticsEvent.LOCAL_AGENT_MESSAGE_CREATED]: BaseProperty;
   [AnalyticsEvent.LOCAL_AGENT_DATA_SOURCE_ATTACHED]: BaseProperty;
-  [AnalyticsEvent.LOCAL_AGENT_DATA_SOURCE_DETACHED]: BaseProperty;
-  [AnalyticsEvent.LOCAL_AGENT_MODEL_CHANGED]: LocalAgentModelChangedProperty;
   [AnalyticsEvent.LOCAL_AGENT_DELETED]: BaseProperty;
   [AnalyticsEvent.CLOUD_DATA_SOURCE_ATTACHED]: BaseProperty;
-  [AnalyticsEvent.CLOUD_DATA_SOURCE_DETACHED]: BaseProperty;
-  [AnalyticsEvent.CLOUD_AGENT_CREATED]: BaseProperty;
-  [AnalyticsEvent.CLOUD_AGENT_VISITED]: BaseProperty;
   [AnalyticsEvent.CLOUD_AGENT_MESSAGE_CREATED]: BaseProperty;
-  [AnalyticsEvent.CLOUD_AGENT_MODEL_CHANGED]: LocalAgentModelChangedProperty;
   [AnalyticsEvent.CLOUD_AGENT_DELETED]: BaseProperty;
-  [AnalyticsEvent.APP_ERROR]: BaseProperty;
   [AnalyticsEvent.ANSWERED_ONBOARDING_SURVEY]: AnsweredOnboardingSurveyProperty;
   [AnalyticsEvent.MOVED_ONBOARDING_STEP]: MovedOnboardingStepProperty;
   [AnalyticsEvent.COMPLETED_ONBOARDING]: BaseProperty;
