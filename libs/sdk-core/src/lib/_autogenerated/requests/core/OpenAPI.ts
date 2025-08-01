878870fa30bd4fa3c583943c51e4752ba9d393ed
Sarah Wooders <sarahwooders@gmail.com>
2025-07-31 17:07:41 -0700

    chore: bump version to 0.10.0 (#3684)

--- libs/sdk-core/src/lib/_autogenerated/requests/core/OpenAPI.ts
+++ libs/sdk-core/src/lib/_autogenerated/requests/core/OpenAPI.ts
@@ -47,7 +47,7 @@ export const OpenAPI: OpenAPIConfig = {
   PASSWORD: undefined,
   TOKEN: undefined,
   USERNAME: undefined,
-  VERSION: '0.9.0',
+  VERSION: '0.10.0',
   WITH_CREDENTIALS: false,
   interceptors: {
     request: new Interceptors(),
