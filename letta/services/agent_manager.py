7c36fd136ba1b4b9c29753127adaf4e7f71c0cb5
cpacker <packercharles@gmail.com>
2025-09-14 18:55:31 -0700

    fix: working again

--- letta/services/agent_manager.py
+++ letta/services/agent_manager.py
@@ -340,6 +340,9 @@ class AgentManager:
                 tool_names.discard("send_message")
                 # NOTE: also overwriting inner_thoughts_in_kwargs to force False
                 agent_create.llm_config.put_inner_thoughts_in_kwargs = False
+                # NOTE: also overwrite initial message sequence to empty by default
+                if agent_create.initial_message_sequence is None:
+                    agent_create.initial_message_sequence = []
             elif agent_create.agent_type == AgentType.workflow_agent:
                 pass  # no default tools
             else:
@@ -544,6 +547,9 @@ class AgentManager:
                 tool_names.discard("send_message")
                 # NOTE: also overwriting inner_thoughts_in_kwargs to force False
                 agent_create.llm_config.put_inner_thoughts_in_kwargs = False
+                # NOTE: also overwrite initial message sequence to empty by default
+                if agent_create.initial_message_sequence is None:
+                    agent_create.initial_message_sequence = []
             elif agent_create.agent_type == AgentType.workflow_agent:
                 pass  # no default tools
             else:
