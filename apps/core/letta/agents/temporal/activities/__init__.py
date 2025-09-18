5d606f02150a1dbebfe6b90b6ecb633c9c803428
cthomas <caren@letta.com>
2025-09-18 16:25:08 -0700

    fix: worker imports (#4790)

--- apps/core/letta/agents/temporal/activities/__init__.py
+++ apps/core/letta/agents/temporal/activities/__init__.py
@@ -1,11 +1,10 @@
+from letta.agents.temporal.activities.create_messages import create_messages
 from letta.agents.temporal.activities.example_activity import example_activity
+from letta.agents.temporal.activities.execute_tool import execute_tool
 from letta.agents.temporal.activities.llm_request import llm_request
-from letta.agents.temporal.activities.message_creation_activities import create_messages_activity
-from letta.agents.temporal.activities.persistence_activities import persist_messages_activity
 from letta.agents.temporal.activities.prepare_messages import prepare_messages
 from letta.agents.temporal.activities.refresh_context import refresh_context_and_system_message
 from letta.agents.temporal.activities.summarize_conversation_history import summarize_conversation_history
-from letta.agents.temporal.activities.tool_execution_activities import execute_tool_activity
 from letta.agents.temporal.activities.update_message_ids import update_message_ids
 
 __all__ = [
@@ -14,8 +13,9 @@ __all__ = [
     "llm_request",
     "summarize_conversation_history",
     "example_activity",
-    "execute_tool_activity",
-    "create_messages_activity",
-    "persist_messages_activity",
+    "execute_tool",
+    "create_messages",
+    "prepare_messages",
+    "refresh_context_and_system_message",
     "update_message_ids",
 ]
