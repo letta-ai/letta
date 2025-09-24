233c2cfe51e1bb3b5113eb1378f4501cf7272e7e
Shubham Naik <shub@letta.com>
2025-09-24 10:28:44 -0700

    feat: agent is thimking [PRO-511] (#4889)
    * feat: agent is thimking
    
    * chore: messages
    
    ---------
    
    Co-authored-by: Shubham Naik <shub@memgpt.ai>

--- libs/ui-ade-components/src/lib/ade/panels/Messages/MessageGroups/MessageGroups.tsx
+++ libs/ui-ade-components/src/lib/ade/panels/Messages/MessageGroups/MessageGroups.tsx
@@ -2,42 +2,49 @@ import {
   Alert,
   HStack,
   IconAvatar,
+  LettaInvaderIcon,
   PersonIcon,
-  SmallInvaderOutlineIcon,
   SystemIcon,
+  ThinkingIcon,
   Typography,
-  VStack
+  VStack,
 } from '@letta-cloud/ui-component-library';
 import React, { memo, useMemo } from 'react';
 import { useGroupedMessages } from '../hooks/useGroupedMessages/useGroupedMessages';
-import type {
-  ListMessagesResponse,
-  ToolReturnMessage
-} from '@letta-cloud/sdk-core';
+import type { ListMessagesResponse, ToolReturnMessage } from '@letta-cloud/sdk-core';
 import type { AgentSimulatorMessageGroupType } from '../../AgentSimulator/types';
 import { Message } from './Message/Message';
 import { useTranslations } from '@letta-cloud/translations';
 import type { MessagesDisplayMode } from '../types';
 import { MessageGroupContextProvider } from '../hooks/useMessageGroupContext/useMessageGroupContext';
 import { deepEqual } from 'fast-equals';
+import { cn } from '@letta-cloud/ui-styles';
 
 interface MessageGroupType {
   group: AgentSimulatorMessageGroupType;
   dataAnchor?: string;
   mode: MessagesDisplayMode;
+  isSendingMessage?: boolean;
 }
 
-function MessageGroup({ group, dataAnchor, mode }: MessageGroupType) {
+function MessageGroup({
+  group,
+  dataAnchor,
+  isSendingMessage,
+  mode,
+}: MessageGroupType) {
   const { name, messages } = group;
 
+  const t = useTranslations('components/Messages');
+
   const sortedMessages = useMemo(() => {
-    let nextMessages = messages.sort(
-      (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-    );
+    let nextMessages = messages.filter((message) => {
+      return message.id !== 'sending-message-placeholder';
+    });
 
     if (mode !== 'debug') {
       return nextMessages.filter(
-        (m) => m.message_type !== 'tool_return_message'
+        (m) => m.message_type !== 'tool_return_message',
       );
     }
 
@@ -56,9 +63,9 @@ function MessageGroup({ group, dataAnchor, mode }: MessageGroupType) {
 
             return acc;
           },
-          {} as Record<string, ToolReturnMessage>
+          {} as Record<string, ToolReturnMessage>,
         ),
-    [messages]
+    [messages],
   );
 
   const textColor = useMemo(() => {
@@ -87,7 +94,13 @@ function MessageGroup({ group, dataAnchor, mode }: MessageGroupType) {
 
   const icon = useMemo(() => {
     if (name === 'Agent') {
-      return <SmallInvaderOutlineIcon size="xsmall" />;
+      if (isSendingMessage) {
+        return (
+          <ThinkingIcon size="xsmall" className="w-[10px]" color="inherit" />
+        );
+      }
+
+      return <LettaInvaderIcon size="xsmall" />;
     }
 
     if (name === 'User') {
@@ -99,19 +112,31 @@ function MessageGroup({ group, dataAnchor, mode }: MessageGroupType) {
     }
 
     return null;
-  }, [name]);
+  }, [name, isSendingMessage]);
 
-  const style = useMemo(
-    () => ({
+  const renderedName = useMemo(() => {
+    if (sortedMessages.length === 0) {
+      return t('agentIsThinking');
+    }
+
+    return name;
+  }, [sortedMessages.length, name, t]);
+
+  const style = useMemo(() => {
+    if (sortedMessages.length === 0) {
+      return {
+        backgroundColor: 'transparent',
+        borderColor: 'hsl(var(--agent-content-background))',
+      };
+    }
+
+    return {
       backgroundColor:
         name === 'User'
           ? 'hsl(var(--user-content-background))'
-          : 'hsl(var(--agent-content-background))'
-    }),
-    [name]
-  );
-
-
+          : 'hsl(var(--agent-content-background))',
+    };
+  }, [name, sortedMessages.length]);
   return (
     <MessageGroupContextProvider key={mode} mode={mode}>
       <VStack
@@ -121,52 +146,65 @@ function MessageGroup({ group, dataAnchor, mode }: MessageGroupType) {
         paddingRight="medium"
         /* eslint-disable-next-line react/forbid-component-props */
         style={style}
-        className="message-group rounded-t-[0.375rem] gap-1.5 rounded-br-[0.375rem] w-full"
+        className="message-group rounded-t-[0.375rem] border border-transparent transition-colors gap-1.5 rounded-br-[0.375rem] w-full"
         data-testid="message-group"
         {...(dataAnchor && { 'data-anchor': dataAnchor })}
       >
-        <HStack align="center" className="gap-1.5">
-          <IconAvatar
-            textColor={textColor}
-            backgroundColor={backgroundColor}
-            icon={icon}
-            size={'xxsmall'}
-            className="rounded-[2px]"
-          />
-          <Typography variant="body4" className="tracking-[0.04em] font-bold">
-            {name.toUpperCase()}
-          </Typography>
+        <HStack justify="spaceBetween" fullWidth>
+          <HStack align="center" className="gap-1.5">
+            <IconAvatar
+              textColor={textColor}
+              backgroundColor={backgroundColor}
+              icon={icon}
+              size={'xxsmall'}
+              className="rounded-[2px]"
+            />
+            <Typography
+              color={sortedMessages.length === 0 ? 'muted' : 'inherit'}
+              uppercase
+              variant="body4"
+              className={cn('tracking-[0.04em] font-bold')}
+            >
+              {renderedName}
+            </Typography>
+          </HStack>
         </HStack>
-        <VStack
-          gap="medium"
-          data-testid={`${name.toLowerCase()}-message-content`}
-        >
-          {sortedMessages.map((message, index, { length }) => {
-            const toolReturnMessage =
-              message.message_type === 'tool_call_message' || message.message_type === 'approval_request_message' &&
-              'tool_call_id' in message.tool_call
-                ? toolReturnMessages[message.tool_call.tool_call_id || '']
-                : undefined;
+        {sortedMessages.length > 0 && (
+          <VStack
+            gap="medium"
+            data-testid={`${name.toLowerCase()}-message-content`}
+          >
+            {sortedMessages.map((message, index, { length }) => {
+              const toolReturnMessage =
+                message.message_type === 'tool_call_message' ||
+                (message.message_type === 'approval_request_message' &&
+                  'tool_call_id' in message.tool_call)
+                  ? toolReturnMessages[message.tool_call.tool_call_id || '']
+                  : undefined;
 
-            const messageToSet = message;
+              const messageToSet = message;
 
-            if (message.message_type === 'tool_call_message' || message.message_type === 'approval_request_message') {
-              // set the stepId of the tool_call_message to the tool_return_message's stepId if it exists
-              if (toolReturnMessage && toolReturnMessage.step_id) {
-                messageToSet.step_id = toolReturnMessage.step_id;
+              if (
+                message.message_type === 'tool_call_message' ||
+                message.message_type === 'approval_request_message'
+              ) {
+                // set the stepId of the tool_call_message to the tool_return_message's stepId if it exists
+                if (toolReturnMessage && toolReturnMessage.step_id) {
+                  messageToSet.step_id = toolReturnMessage.step_id;
+                }
               }
-            }
 
-            return (
-              <Message
-                isLastMessage={index === length - 1}
-                key={`${message.id}_${index}`}
-                message={messageToSet}
-                toolReturnMessage={toolReturnMessage}
-              />
-            );
-          })}
-        </VStack>
+              return (
+                <Message
+                  isLastMessage={index === length - 1}
+                  key={`${message.id}_${index}`}
+                  message={messageToSet}
+                  toolReturnMessage={toolReturnMessage}
+                />
+              );
+            })}
+          </VStack>
+        )}
       </VStack>
     </MessageGroupContextProvider>
   );
@@ -176,33 +214,39 @@ interface MessageGroupsProps {
   messages: ListMessagesResponse;
   hasNextPage?: boolean;
   mode: MessagesDisplayMode;
+  isSendingMessage?: boolean;
 }
 
-export const MessageGroups = memo(function MessageGroups(props: MessageGroupsProps) {
-  const { messages, mode, hasNextPage = false } = props;
+export const MessageGroups = memo(
+  function MessageGroups(props: MessageGroupsProps) {
+    const { messages, mode, isSendingMessage, hasNextPage = false } = props;
 
-  const t = useTranslations('components/Messages');
+    const t = useTranslations('components/Messages');
 
-  const messageGroups = useGroupedMessages({
-    messages
-  });
+    const messageGroups = useGroupedMessages({
+      messages,
+    });
 
-
-  return (
-    <>
-      {messageGroups.map((group, index) => (
-        <MessageGroup
-          mode={mode}
-          key={index}
-          group={group}
-          dataAnchor={index === 0 ? 'old-first' : undefined}
-        />
-      ))}
-      {hasNextPage && messageGroups.length === 0 && mode === 'simple' && (
-        <Alert variant="info" title={t('noParsableMessages')} />
-      )}
-    </>
-  );
-}, (prevProps, nextProps) => {
-  return deepEqual(prevProps, nextProps);
-});
+    return (
+      <>
+        {messageGroups.map((group, index) => (
+          <MessageGroup
+            isSendingMessage={
+              index === messageGroups.length - 1 && isSendingMessage
+            }
+            mode={mode}
+            key={index}
+            group={group}
+            dataAnchor={index === 0 ? 'old-first' : undefined}
+          />
+        ))}
+        {hasNextPage && messageGroups.length === 0 && mode === 'simple' && (
+          <Alert variant="info" title={t('noParsableMessages')} />
+        )}
+      </>
+    );
+  },
+  (prevProps, nextProps) => {
+    return deepEqual(prevProps, nextProps);
+  },
+);
