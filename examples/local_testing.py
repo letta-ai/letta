58f9151ba9420678dc2a9d358135e84403b5b9eb
Pranav Ramesh <pranavramesh@Pranavs-MacBook-Air.local>
2025-10-03 15:14:15 -0700

    finished per agent mcp tool override

--- examples/local_testing.py
+++ examples/local_testing.py
@@ -1,34 +1,14 @@
 from letta_client import Letta
 from pprint import pprint
-from letta.agents.agent_loop import AgentLoop
-from letta.agents.letta_agent_v2 import LettaAgentV2
-from letta.schemas.message import Message, MessageCreate, MessageUpdate, MessageRole
-from letta.schemas.letta_request import (
-    LettaAsyncRequest,
-    LettaRequest,
-    LettaStreamingRequest,
-)
-from letta.functions.mcp_client.types import (
-    MCPTool,
-    SSEServerConfig,
-    StdioServerConfig,
-    StreamableHTTPServerConfig,
-)
-from letta.schemas.mcp import (
-    UpdateSSEMCPServer,
-    UpdateStdioMCPServer,
-    UpdateStreamableHTTPMCPServer,
-)
 
+########################################
+# Set Up (leave this portion Uncommented during testing in both the per tool and per agent scenarios)
+'''
+For testing/demonstrating MCP tool overrides for each case, simply comment out the version you don't want to test.
+Leave the client and new_input_schema instantiations uncommented for ease of use.
+'''
+########################################
 client = Letta(base_url="http://localhost:8283")
-# # List tools from an MCP server
-# tools = client.tools.list_mcp_tools_by_server(mcp_server_name="deep_wiki_server")
-# print(tools[0])
-
-list_tools= client.tools.list()
-# # CASE 1: Tool already exists in Letta server and we just update schema
-# # CASE 2: Tool doesn't exist in Letta server but exists in MCP server, BUT when we add from MCP to Letta, we want to override the schema of the MCP tool
-
 new_input_schema = {
     "$schema": "http://json-schema.org/draft-06/schema#",
     "additionalProperties": False,
@@ -42,97 +22,88 @@ new_input_schema = {
     "type": "object",
 }
 
-# # Add a specific tool from the MCP server but override schema with the user provided one
-# # CASE 2: Tool doesn't exist in Letta server but exists in MCP server, BUT when we add from MCP to Letta, we want to override the schema of the MCP tool
-#override schema with new input
+########################################
+# Per Tool MCP Schema Override
+########################################
 
-#add tool with normal schema
-# tool = client.tools.add_mcp_tool(
-#     mcp_server_name="deep_wiki_server",
-#     mcp_tool_name="read_wiki_structure",
-# )
+## Case 1: Overriding Schema for an Existing MCP Tool in Letta Server ###
 
-# tools = client.tools.list()
-# print(tools[0].json_schema)
+# add MCP tool from MCP Server to Letta Server (default tool schema)
+tool = client.tools.add_mcp_tool(
+    mcp_server_name="deep_wiki_server",
+    mcp_tool_name="read_wiki_structure",
+)
 
-#does an upsert
-pprint(client.tools.list(name=))
+#example of what schema looks like
+print("Original Schema:")
+pprint(tool.json_schema)
+
+#now override schema with custom schema for MCP Tool
 tool_overridden = client.tools.add_mcp_tool_override(
     mcp_server_name="deep_wiki_server",
     mcp_tool_name="read_wiki_structure",
     overridden_schema=new_input_schema,
 )
+
+print("Updated (overridden) Schema:")
 pprint(tool_overridden.json_schema)
 
-# tool is edited on letta's server side but not on mcp server side
-tools = client.tools.list()
-rtools = client.tools.retrieve(tool_overridden.id)
-pprint(tools[0].json_schema)
-pprint(tools[0].id) #'tool-c24b425b-e9b4-4a9b-bfbd-1b86e40ea340'
-pprint(rtools.json_schema)
-pprint(rtools.id) # 'tool-af73ea02-15ba-475b-b317-0ed82af60aa5'
-pprint(len(client.tools.list()))
-# updated_tools = client.tools.list_mcp_tools_by_server(mcp_server_name="deep_wiki_server")
-# pprint(updated_tools[0])
+#validate that updated schema persisted
+retrieve_updated_mcp_tool = client.tools.retrieve(tool_overridden.id)
+print("Retrieved Updated Schema:")
+pprint(retrieve_updated_mcp_tool.json_schema)
+print("Did overridden schema persist:", retrieve_updated_mcp_tool.json_schema == tool_overridden.json_schema)
 
-# BE CAREFUL HERE db backend is updated and letta mcp toolset is updated but need to update the mcp tool itself 
-# response = client.tools.update_mcp_server(
-#     mcp_server_name="deep_wiki_server",
-#     request=UpdateStreamableHTTPMCPServer(
-#         server_url='https://mcp.deepwiki.com/mcp'
-#     ),
-# )
-#see tools in server now
-# updated_tools = client.tools.list_mcp_tools_by_server(mcp_server_name="deep_wiki_server")
-# pprint(updated_tools[0])
+### Case 2: Adding a New MCP Tool with Overridden Schema to Letta Server ###
 
-# updated_tools = client.tools.list_mcp_tools_by_server(mcp_server_name="deep_wiki_server")
-# pprint(f"New Input Schema: {tools[0].input_schema}")
-# pprint(type(tool))
-# pprint(tool.json_schema)
+#can automatically add the new MCP tool and override the MCP tool schema in one shot
+tool_overridden = client.tools.add_mcp_tool_override(
+    mcp_server_name="deep_wiki_server",
+    mcp_tool_name="read_wiki_structure",
+    overridden_schema=new_input_schema,
+)
 
-# #Create agent with MCP tool attached
-# agent_state = client.agents.list()[0]
-# agent_loop = AgentLoop.load(agent_state=agent_state, actor="User")
-# # response = agent_loop.build_request(
-# #     input_messages=[message]
-# # )
+print("Updated (overridden) Schema:")
+pprint(tool_overridden.json_schema)
 
-# message = MessageCreate(
-#         role=MessageRole.user,
-#         content="Use the read_wiki_structure_tool to analyze the directory structure of facebook/react"
-#     )
-# r = LettaRequest(
-#     messages=[message]
-# )
-# pprint(f"Request type: {type(r)}")
-# response = client.agents.messages.preview_raw_payload(
-#     agent_id="agent-edfd4884-1c98-459a-abee-7996e0ae743c",
-#     request=LettaRequest(
-#             messages=[
-#             MessageCreate(
-#             role="user",
-#             content="Use the read_wiki_structure_tool to analyze the directory structure of facebook/react",
-#             )
-#             ],
-#             ),
+#validate that updated schema persisted
+retrieve_updated_mcp_tool = client.tools.retrieve(tool_overridden.id)
+print("Retrieved Updated Schema:")
+pprint(retrieve_updated_mcp_tool.json_schema)
+print("Did overridden schema persist:", retrieve_updated_mcp_tool.json_schema == tool_overridden.json_schema)
 
-# )
-# pprint(response)
 
-# # Or attach tools to an existing agent
-# # client.agents.tools.attach(
-# #     agent_id='agent-edfd4884-1c98-459a-abee-7996e0ae743c',
-# #     tool_id=tool.id
-# # )
+########################################
+# Per Agent MCP Tool Schema Override
+########################################
 
-# # #Use the agent with MCP tools
-# # response = client.agents.messages.create(
-# #     agent_id=agent_state.id,
-# #     messages=[
-# #         {
-# #             "role": "user",
-# #             "content": "Use the read_wiki_structure tool to check the repo structure of facebook/react"
-# #         }
-# #     ]
-# # )
+#add an MCP tool to the Letta Server
+mcp_tool = client.tools.add_mcp_tool(
+    mcp_server_name="deep_wiki_server",
+    mcp_tool_name="read_wiki_structure",
+)
+
+#attach the MCP tool to the Agent
+client.agents.tools.attach(
+    agent_id="agent-735f2d68-b0a7-4eed-a3bd-75abc911f571",
+    tool_id=f"{mcp_tool.id}"
+)
+
+print(f"Original Schema for {mcp_tool.name}: ")
+pprint(mcp_tool.json_schema)
+
+# override this MCP tool schema (updates core backend DB so other agents sharing this tool will also have the updated schema)
+client.agents.tools.override_mcp_tool(
+    agent_id="agent-735f2d68-b0a7-4eed-a3bd-75abc911f571",
+    tool_id=f"{mcp_tool.id}",
+    overridden_schema=new_input_schema
+)
+
+# grab the specific MCP tool who's schema was overridden
+for t in client.agents.tools.list(agent_id='agent-735f2d68-b0a7-4eed-a3bd-75abc911f571'):
+    if t.id == mcp_tool.id:
+        inspect_tool = t
+        break
+
+print(f"Overridden Schema for {inspect_tool.name}: ")
+pprint(inspect_tool.json_schema)
